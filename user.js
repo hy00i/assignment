54bde5f7f53142fb940664843d7e33b8898a790f
nurulfarina <101855108+nurulfarina@users.noreply.github.com>
2022-05-11 01:28:58 +0800

    Add files via upload

--- user.js
+++ user.js
@@ -1,50 +1,35 @@
 let users;
-
 class User {
 	static async injectDB(conn) {
-		users = await conn.db("Visitor-management-System").collection("users")
+		users = await conn.db("my-database-name").collection("users")
 	}
 
 	static async register(username, password) {
 		// TODO: Check if username exists
-		return users.findOne({
-			'username':username
-		}).then(async user=>{
-			if (user) {
-				if(user.username==username){
-					return "the Username is already exist";
-				}
-				
-			} else {
 
-			await users.insertOne({
-				"username": username, 
-				"password": password,
-			})
-			return "user created";
-			}
-		})
+		// TODO: Hash password
 
-	}
+		// TODO: Save user to database
+     
 
 	static async login(username, password) {
-		return users.findOne({
-			'username':username
-		}).then(async user=>{
-			if(user){
-				if(user.password!=password){
-					return "invalid password";
-				}
-				else{
-					return "login successful";
-				}	
+		// TODO: Check if username exists
+		let user = await users.findOne({ "username": username });
+		// TODO: Validate password
+		if (user) {
+			const verified = await bcrypt.compare(password,user.Password)
+			if (verified)
+			{
+		// TODO: Return user object
+				return user
 			}
 			else{
-				return "invalid username";
+				return "The password is invalid"
 			}
-			
-		})
-
+		}
+		else{
+			return "Username is invalid"
+		}
 	}
 }
  
