327142dd241c8665176bf3a2081d33942de9f43d
nurulfarina <101855108+nurulfarina@users.noreply.github.com>
2022-06-18 20:48:54 +0800

    Update visitor.js

--- visitor.js
+++ visitor.js
@@ -3,23 +3,41 @@ const ObjectId = require("mongodb").ObjectId;
 let visitors;
 class Visitor {
 	static async injectDB(conn) {
-		visitors = await conn.db("vms").collection("visitors")
+		visitors = await conn.db("Visitor-management-System").collection("visitors")
 	}
 
-	static async getVisitor(id) {
+	static async getVisitor(visitorId) {
         // TODO - READ Visitors from DB
+		return await visitors.findOne({ _id: new ObjectId(visitorId) })
 	}
-
-	static async createVisitor(name, id, department, date, input) {
+    
+	static async createVisitor(name, department, date, ) {
         // TODO - CREATE Visitors in DB
+        await visitors.insertOne({
+            "name":name,
+            "department":department,
+            "date":date,
+        })
+
 	}
 
-	static async updateVisitor(id, department) {
+	static async updateVisitor(_id, department) {
         // TODO - UPDATE Visitors in DB
+        await visitors.updateOne(
+            {_id:ObjectId},
+            {
+                $set:{
+                    "department":department
+                }
+            }
+        )
 	}
 
 	static async deleteVisitor(id) {
         // TODO - DELETE Visitors in DB
+        await visitors.deleteOne({
+            "_id":ObjectId
+        })
 	}
 	
 }
